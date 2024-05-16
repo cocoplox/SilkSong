ed76124a4d296a0b6cd8e498bf92bc695650fe08
Hydrucs <jordi.roman@nostrasenyora.epiaedu.cat>
2024-05-16 12:36:40 +0200

    padro

---
+++ Assets/Scripts/Juego/AscensorFinal.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AscensorFinal : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float moveSpeed = 4f;
+    public int patrolDestination;
+    private bool playerTouched = false;
+
+    private void Update()
+    {
+        if (playerTouched)
+        {
+            if (patrolDestination == 0)
+            {
+                Vector3 targetPosition = new Vector3(transform.position.x, waypoints[0].position.y, transform.position.z);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+                if (Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.position.x, waypoints[0].position.y)) < .1f)
+                {
+                    //transform.localScale = new Vector3(-1, 1, 1);
+                    patrolDestination = 1;
+                }
+            }
+            else if (patrolDestination == 1)
+            {
+                Vector3 targetPosition = new Vector3(transform.position.x, waypoints[1].position.y, transform.position.z);
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+                if (Vector2.Distance(new Vector2(transform.position.x, transform.position.y), new Vector2(transform.position.x, waypoints[1].position.y)) < .1f)
+                {
+                    //transform.localScale = new Vector3(1, 1, 1);
+                    patrolDestination = 0;
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            playerTouched = true;
+        }
+    }
+}
