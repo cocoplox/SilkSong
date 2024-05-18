41606a674dcab4233db76b2f58c5e66620e67fc1
cocoplox <ivanvegasantaella5@gmail.com>
2024-05-18 21:24:07 +0200

    Inventario casi terminado

---
+++ Assets/InventarioManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class InventarioManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text nivelEspada;
+    [SerializeField] TMP_Text vidaMaxima;
+    [SerializeField] GameObject garrasImagen;
+    [SerializeField] GameObject alasMonarca;
+
+    private void Start()
+
+    {
+        garrasImagen.SetActive(false);
+        alasMonarca.SetActive(false);
+
+        nivelEspada.SetText("Level  " + Variables.nivelEspada);
+        vidaMaxima.SetText("X " + Variables.vidaMaxima);
+
+    }
+
+    void Update()
+    {
+        if (Variables.isGarras == true) { garrasImagen.SetActive(true); }
+        if (Variables.isAlas == true) {  alasMonarca.SetActive(true);}
+
+        nivelEspada.SetText("Level  " + Variables.nivelEspada);
+        vidaMaxima.SetText("X " + Variables.vidaMaxima);
+
+
+    }
+}
+    
